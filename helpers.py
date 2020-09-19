3b0afbe993e73c25154541ba6c89c588e7437b49
White <jtwhite1000@gmail.com>
2020-09-19 12:12:47 -0600

    finally unwrapped mf6 high res arrays - not sure why flopy isnt doing it now

--- helpers.py
+++ helpers.py
@@ -14,3 +14,27 @@ def replace_time_with_datetime(csv_file):
     return new_file,df
 
 
+def unwrap_mf6_arrays(org_d,new_d,nrow=120,ncol=60):
+    import os
+    import shutil
+    import numpy as np
+
+    assert os.path.exists(org_d),org_d
+    if os.path.exists(new_d):
+        shutil.rmtree(new_d)
+    shutil.copytree(org_d,new_d)
+
+    arr_files = [f for f in os.listdir(new_d) if "_layer" in f and f.endswith(".txt")]
+    print(arr_files)
+    for arr_file in arr_files:
+        arr = np.loadtxt(os.path.join(new_d,arr_file)).reshape((nrow,ncol))
+        fmt = "%15.6E"
+        if "idomain" in arr_file:
+            fmt = "%2d"
+
+        np.savetxt(os.path.join(new_d,arr_file),arr,fmt=fmt)
+
+
+
+if __name__ == "__main__":
+    unwrap_mf6_arrays("temp_daily_test","unwrapped")
